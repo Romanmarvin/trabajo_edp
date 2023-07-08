bb0be33dea48d62475c66cc62b7046011ec438b4
Román Hugo <136202507+Romanmarvin@users.noreply.github.com>
2023-07-08 18:40:32 -0300

    refactor(nuevo)

---
+++ comprimir.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+imagenes=$(ls *.jpg )
+echo "$imagenes"> imagenes.txt
+
+archivo="/home/marvin/trabajo_practico/imagenes.txt"
+salida="/home/marvin/trabajo_practico/femeninas.txt"
+while IFS= read -r linea; do
+    palabras=($linea)
+        
+    palabra_terminada_en_a=false
+    
+    # Recorrer las palabras
+    for palabra in "${palabras[@]}"; do
+        # Verificar si la palabra termina en a seguida de un espacio o una coma
+        if [[ $palabra =~ [aA]$ | $palabra =~ [aA],[[:space:]] ]]; then
+            palabra_terminada_en_a=true
+            break
+        fi
+    done
+    
+    # Copiar la línea completa si se encuentra una palabra terminada en "a"
+    if $palabra_terminada_en_a; then
+        echo "$linea" >> "$salida"
+    fi
+done < "$archivo"
+
+#echo "Proceso completado $salida"
+
+carp="/home/marvin/trabajo_practico"
+
+find "$carp" -type f ! -name "*.sh" -exec tar -czvf archivos.tar.gz {} +
+
+#tar -czvf arc.tar.gz $arc
+
+echo "archivos comprimidos en arc.tar.gz"
