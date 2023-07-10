9b8dd0eb755cd408a3b88058c557fcab14813d92
ignaciobaclini <104173146+ignaciobaclini@users.noreply.github.com>
2023-07-10 01:03:47 -0300

    update
    se agrega el script de los procesos de imagenes

---
+++ procesar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Directorio de búsqueda
+directorio="/home/marvin/trabajo_practico"
+#se usa el find buscar , el grep para filtrar  
+for archivo in $(find "$directorio" -type f -name "*.jpg" | grep -E '[A-Z][a-z]+');do
+    # Obtener la ruta completa del archivo
+    ruta_archivo=$(dirname "$archivo")
+    nombre=$(basename "$archivo")
+    # le saco los numeros
+    salida=$(echo "$nombre" | sed 's/[0-9]*//g')
+
+    ruta_salida="$ruta_archivo/$salida"
+
+    convert "$archivo" -gravity center -resize 512x512+0+0 -extent 512x512 "$ruta_salida"
+
+    #echo "Transformación aplicada a: $archivo"
+    #echo "Imagen transformada guardada en: $ruta_salida"
+done
+
+echo "Transformación completa."
+
+#nose como pero anda
