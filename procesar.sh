01a3a5d1b313b62d5d35aba1b23def5892069206
Román Hugo <136202507+Romanmarvin@users.noreply.github.com>
2023-07-19 00:50:08 -0300

    verificación

--- procesar.sh
+++ procesar.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 # Directorio de búsqueda
-directorio="/trabajo_edp"
-#se usa el find buscar , el grep para filtrar  
-find "$directorio" -type f -name "*.jpg" | grep -E '[A-Z][a-z]+' | while IFS= read -r archivo; do
+if find . -type f -name "*.jpg" -print -quit; then
+    directorio="/trabajo_edp"
+    #se usa el find buscar , el grep para filtrar  
+    find "$directorio" -type f -name "*.jpg" | grep -E '[A-Z][a-z]+' | while IFS= read -r archivo; do
     # Obtener la ruta completa del archivo
-    ruta_archivo=$(dirname "$archivo")
-    nombre=$(basename "$archivo")
-    # le saco los numeros
-    salida=$(echo "$nombre" | sed 's/[0-9]*//g')
+        ruta_archivo=$(dirname "$archivo")
+        nombre=$(basename "$archivo")
+        # le saco los numeros
+        salida=$(echo "$nombre" | sed 's/[0-9]*//g')
 
-    ruta_salida="$ruta_archivo/$salida"
+        ruta_salida="$ruta_archivo/$salida"
 
-    convert "$archivo" -gravity center -resize 512x512+0+0 -extent 512x512 "$ruta_salida"
+        convert "$archivo" -gravity center -resize 512x512+0+0 -extent 512x512 "$ruta_salida"
 
     #echo "Transformación aplicada a: $archivo"
     #echo "Imagen transformada guardada en: $ruta_salida"
-done
+    done
 
-echo "Transformación completa."
+    echo "Transformación completa."
 
+else
+  echo "No hay imágenes en el directorio. Genera las imágenes primero."
+fi
 #nose como pero anda
