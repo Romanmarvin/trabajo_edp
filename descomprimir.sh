9a685cfbb31dd8f53257edf8b1dc08af7d5f057f
Román Hugo <136202507+Romanmarvin@users.noreply.github.com>
2023-07-19 00:52:35 -0300

    validación

--- descomprimir.sh
+++ descomprimir.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-#direccion donde esta el .tar
-imagenes="/trabajo_edp/imagenes.tar.gz"
-#direccion del txt
-sum="/trabajo_edp/suma.txt"
-#descomprimir , con -c cambiar al directorio final 
-tar -xzvf "$imagenes" 
-carpeta_imagenes=$(tar -tzf "$imagenes")
-if md5sum -c suma.txt | grep -q "OK"; then
-    echo "igualitos"
+if find . -type f -name "*.tar.gz" -print -quit; then
+    #direccion donde esta el .tar
+    imagenes="/trabajo_edp/imagenes.tar.gz"
+    #direccion del txt
+    sum="/trabajo_edp/suma.txt"
+    #descomprimir , con -c cambiar al directorio final 
+    tar -xzvf "$imagenes" 
+    carpeta_imagenes=$(tar -tzf "$imagenes")
+    if md5sum -c suma.txt | grep -q "OK"; then
+        echo "igualitos"
+    else
+        echo "ocurrio un error" 
+    fi 
 else
-    echo "ocurrio un error" 
-fi 
+  echo "Genera las imagenes primero!!"
+fi
 #RTFM 
 
 
